fe863a838a1c7d4d9dc249dca2a6f746e29c291c
Ruma Das <rumaadl16@gmail.com>
2020-05-10 23:12:05 +0930

    modified readme file in server.js

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+
 const app = express();
+
+
+
 const PORT = process.env.PORT || 3000;
 
-const noteArr =[];
+let noteArr =[];
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
@@ -14,30 +18,58 @@ app.listen(PORT, function() {
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("*",(req, res) =>{
-    res.sendFile(path.join(__dirname,"/public/index.html"));
+app.get("/notes",(req, res) =>{
+    res.sendFile(path.join(__dirname,"public/notes.html"));
 });
 
-app.get("/notes",(req, res) =>{
-    res.sendFile(path.join(__dirname,"/public/notes.html"));
+app.get("/assets/css/styles.css",(req, res) =>{
+    res.sendFile(path.join(__dirname,"public/assets/css/styles.css"));
+});
+
+app.get("/assets/js/index.js",(req, res) =>{
+    res.sendFile(path.join(__dirname,"public/assets/js/index.js"));
 });
 
 app.get("/api/notes", (req, res) => {
-    res.json({
-        notes: noteArr
-    });
+//     fs.readFile(path.join(__dirname,"db/db.json"), "utf8",((err, data) =>{
+//     console.log(noteArr);
+//     res.send(noteArr);
+//   }));
+  fs.readFile(path.join(__dirname, "db/db.json"), 'utf8', (error, data) => {
+    if (error) {
+      // oh no!
+      // (you should do something here, like res.status(500) maybe.
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    else {
+      noteArr = JSON.parse(data);
+    }
+  });
 });
 
 app.post("/api/notes",(req, res) =>{
-    const notes= req.body;
-    noteArr.push(notes);
-    res.send(JSON.stringify(noteArr));
+    //    fs.writeFile(path.join(__dirname,"db/db.json"), "utf8",((err, data) =>{
+    //     noteArr = JSON.parse(data);
+    //     console.log(noteArr);
+    //     res.send(noteArr);
+    //   }));
+      fs.writeFile(path.join(__dirname, 'etc'), JSON.stringify(noteArr), (error) => {
+        if (error) { 
+          // oh no!
+        }
+        else {
+          res.send({});
+        }
+      });
 
 });
 
 app.delete("/api/notes/:id", (req, res) =>{
     res.send();
     noteArr.length =0;
-
    
 })
+app.get("*",(req, res) =>{
+    res.sendFile(path.join(__dirname,"public/index.html"));
+});
