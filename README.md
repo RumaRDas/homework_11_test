bada53468110a2181c7604c087034e71c0d54c92
Ruma Das <rumaadl16@gmail.com>
2020-05-14 00:41:29 +0930

    Changing in readme and server.js

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
-# Homework: Note Taker
+# : Note Taker
 
-## an application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file.
+## description
 
-[View](https://rumardas.github.io/homework_11_test/)
+Generate Team is a dynamically  generates ` Note Taker `   application. Node.js is used in this application And for run this app, ` NPM `  ,`path`, `uuid/v4` needs to install. Deploued to Heroku.
+
+In this application users can save note  and open note for read in db.json file. Users can delete any othay wants.
+____
+
+   [View](https://rumardas.github.io/noteTaker/)
+
+##  Applications Uses in this projects
+    * Html
+    * css
+    * Java Script
+    * Node.js
+
+## Instalation 
+
+        $ npm install, 
+        $ npm install gitignore -g
+        $ npm install uuid4
+
+
+[Heroku Link](https://drive.google.com/file/d/1PqGyyB_0aFpz0nCpkar_aOfLTUyggktc/view/)
+
+## page view
+
+![Page view](view.png)
+
--- server.js
+++ server.js
@@ -1,71 +1,99 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-
+const uuidv4 = require("uuid/v4");
 
 const app = express();
 
-
-
 const PORT = process.env.PORT || 3000;
 
-let noteArr =[];
-
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
-});
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/notes",(req, res) =>{
-    res.sendFile(path.join(__dirname,"public/notes.html"));
-});
+let noteArr = [];
 
-app.get("/assets/css/styles.css",(req, res) =>{
-    res.sendFile(path.join(__dirname,"public/assets/css/styles.css"));
-});
-
-app.get("/assets/js/index.js",(req, res) =>{
-    res.sendFile(path.join(__dirname,"public/assets/js/index.js"));
-});
-
+//Get  note from db.json
 app.get("/api/notes", (req, res) => {
-// Reade file from db.json file
+  //Reade file from db.json file
   fs.readFile(path.join(__dirname, "db/db.json"), 'utf8', (error, data) => {
     if (error) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
+      throw error;
     }
     else {
       noteArr = JSON.parse(data);
+      return res.json(noteArr);
     }
   });
-  res.json(noteArr);
 });
 
-app.post("/api/notes",(req, res) =>{
-    //    fs.writeFile(path.join(__dirname,"db/db.json"), "utf8",((err, data) =>{
-    //     noteArr = JSON.parse(data);
-    //     console.log(noteArr);
-    //     res.send(noteArr);
-    //   }));
-      fs.writeFile(path.join(__dirname, 'etc'), JSON.stringify(noteArr), (error) => {
-        if (error) { 
-          // oh no!
-        }
-        else {
-          res.send({});
-        }
-      });
+//write note to db.json
+app.post("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "db/db.json"), 'utf8', (error, data) => {
+    if (error) {
+      throw error;
+    }
+    noteArr = JSON.parse(data);
+    let newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4()
+    };
+    noteArr.push(newNote);
+    fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(noteArr), (error) => {
+      if (error) {
+        // oh no!
+        throw error;
+      }
+      return res.json(noteArr);
+      //res.json(JSON.parse(noteArr));
 
+    });
+  });
 });
 
-app.delete("/api/notes/:id", (req, res) =>{
-    res.send();
-    noteArr.length =0;
-   
+//DELETE NOTE FROM DB.JSON
+app.delete("/api/notes/:id", (req, res) => {
+  fs.readFile(path.join(__dirname, "db/db.json"), 'utf8', (error, data) => {
+    if (error) {
+      throw error;
+    }
+    console.log(req.params.id); //req.params.id X data.id
+    noteArr = JSON.parse(data);
+    let result = noteArr.filter(note => note.id != req.params.id);
+    fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(result), (error) => {
+      if (error) {
+        // oh no!
+        throw error;
+      }
+      return res.json(result);
+      //res.json(JSON.parse(noteArr));
+
+    });
+  });
+
 })
-app.get("*",(req, res) =>{
-    res.sendFile(path.join(__dirname,"public/index.html"));
+
+//getting style sheet 
+app.get("/assets/css/styles.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/assets/css/styles.css"));
+});
+
+//getting js file 
+app.get("/assets/js/index.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/assets/js/index.js"));
+});
+
+//Getting page of notes.html
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+//Getting page of index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// start server
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
