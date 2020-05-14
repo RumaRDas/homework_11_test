9e6db0d439449ea8cd023f61ee83714231c79b60
Ruma Das <rumaadl16@gmail.com>
2020-05-14 20:47:56 +0930

    deployed in Heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const uuidv4 = require("uuid/v4");
+//const uuidv4 = require("uuid/v4");
+const {v4: uuidv4}= require("uuid");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
